9ae9b2a9133368f99b954a70fae46d2d8f774b2f
Augusto Dantas <augustodantas.dev@gmail.com>
2025-03-29 22:16:57 -0300

    Show a list with only the most popular characters

--- src/App.jsx
+++ src/App.jsx
@@ -3,56 +3,179 @@ import axios from "axios";
 import CryptoJS from "crypto-js";
 
 function App() {
-	const [character, setCharacter] = useState([]);
+  const [characters, setCharacters] = useState([]);
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-	useEffect(() => {
-    const fetchCharacters = async () => {
+  const topCharacterIds = [
+    1009610, // Spider-Man
+    1009220, // Captain America
+    1009368, // Iron Man
+    1009664, // Thor
+    1009351, // Hulk
+    1009189, // Black Widow
+    1009338, // Hawkeye
+    1009282, // Doctor Strange
+    1009417, // Loki
+    1009697, // Vision
+    1010743, // Groot
+    1009515, // Punisher
+    1009268, // Deadpool
+    1009718, // Wolverine
+    1009262, // Daredevil
+    1009592, // Silver Surfer
+    1009652, // Thanos
+    1009480, // Moon Knight
+    1009504, // Professor X
+    1009257, // Cyclops
+    1009243, // Colossus
+    1009629, // Storm
+    1009472, // Nightcrawler
+    1009546, // Rogue
+    1009175, // Beast
+    1009499, // Polaris
+    1009512, // Psylocke
+    1017577, // Ms. Marvel (Kamala Khan)
+    1010744, // Rocket Raccoon
+    1011299, // Guardians of the Galaxy
+    1010351, // Hulkling
+    1009507, // Quicksilver
+    1009562, // Scarlet Witch
+    1010338, // Captain Marvel (Carol Danvers)
+    1009187, // Black Panther
+    1009471, // Nick Fury
+    1009239, // Cloak
+    1009249, // Dagger
+    1009297, // Falcon
+    1009148, // Absorbing Man
+    1011334, // Winter Soldier
+    1011007, // Ant-Man (Scott Lang)
+    1010802, // Ant-Man (Hank Pym)
+    1009310, // Emma Frost
+    1009662, // Thing
+    1009366, // Invisible Woman
+    1009459, // Mr. Fantastic
+    1009356, // Human Torch
+    1009524, // Quicksilver
+    1009324, // Gambit
+    1009599, // Spider-Woman
+    1009708, // Mary Jane Watson
+    1009372, // J. Jonah Jameson
+    1009477, // Nova
+    1011358, // Nova (Sam Alexander)
+    1009539, // Rhino
+    1009276, // Doctor Octopus
+    1009287, // Electro
+    1014985, // Scarlet Spider (Kaine)
+    1009391, // Kraven the Hunter
+    1009404, // Lizard
+    1011426, // Mysterio
+    1009325, // Norman Osborn
+    1009486, // Modok
+    1010763, // Gamora
+    1010365, // Nebula
+    1011490, // Hank Pym
+    1009481, // Moondragon
+    1009451, // Mary Jane Watson
+    1010733, // Star-Lord (Peter Quill)
+    1009523, // Punisher
+    1009608, // Spider-Girl (May Parker)
+    1009719, // Wonder Man
+    1009680, // Ultron
+    1009722, // Wasp
+    1009726, // X-23
+    1009322, // Galactus
+    1009639, // Super-Skrull
+    1009234, // Chameleon
+    1010373, // Howard the Duck
+    1011023, // Sentry
+    1009407, // Lockheed
+    1017583, // Ms. America
+    1010330, // She-Hulk (Jennifer Walters)
+  ];
+
+  useEffect(() => {
+    const fetchTopCharacters = async () => {
+      setLoading(true);
       const publicKey = import.meta.env.VITE_PUBLIC_API_KEY;
       const privateKey = import.meta.env.VITE_PRIVATE_API_KEY;
-
       const ts = new Date().getTime();
       const hash = CryptoJS.MD5(ts + privateKey + publicKey).toString();
-
+      
       try {
-        const response = await axios.get(
-          `https://gateway.marvel.com/v1/public/characters`,
-          {
-            params: {
-              ts,
-              apikey: publicKey,
-              hash,
-              limit: 10,
-            },
-          }
-				);
-				setCharacter(response.data.data.results)
+        // Buscar cada personagem individualmente por ID
+        const charactersPromises = topCharacterIds.map(id => 
+          axios.get(
+            `https://gateway.marvel.com/v1/public/characters/${id}`,
+            {
+              params: {
+                ts,
+                apikey: publicKey,
+                hash,
+              },
+            }
+          )
+        );
         
-      } catch (error) {
-				console.error("Error fetching characters:", error);
+        const results = await Promise.all(charactersPromises);
+        const fetchedCharacters = results.map(res => res.data.data.results[0]);
+        setCharacters(fetchedCharacters);
 
-			}
+      } catch (error) {
+        console.error("Error fetching characters:", error);
+				
+      } finally {
+        setLoading(false);
+      }
     };
 
-		fetchCharacters();
+    fetchTopCharacters();
   }, []);
 
+  const handleCharacterClick = (character) => {
+    setSelectedCharacter(character);
+  };
+
   return (
-		<>
-			<h1>Marvel</h1>
-			<ul>
-				{character.map((character) => (
-					<li key={character.id}>
-						<h2>{character.name}</h2>
-						<img
-              src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-              alt={character.name}
-            />
-					</li>
-				))}
-			</ul>
-		</>
-	)
-	
+    <div className="marvel-app">
+      <h1>Top 100 Personagens da Marvel</h1>
+      
+      {loading ? (
+        <p>Carregando personagens principais...</p>
+      ) : (
+        <div className="characters-container">
+          <div className="characters-list">
+            <h2>Lista de Personagens</h2>
+            <ul>
+              {characters.map((character) => (
+                <li key={character.id}>
+                  <a href="#" onClick={() => handleCharacterClick(character)}>
+                    {character.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          {selectedCharacter && (
+            <div className="character-details">
+              <h2>{selectedCharacter.name}</h2>
+              <img
+                src={`${selectedCharacter.thumbnail.path}.${selectedCharacter.thumbnail.extension}`}
+                alt={selectedCharacter.name}
+              />
+              {selectedCharacter.description && (
+                <p>{selectedCharacter.description}</p>
+              )}
+              <p>
+                <strong>Aparições em quadrinhos:</strong> {selectedCharacter.comics.available}
+              </p>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
